99192df02a16272478deac9fe401c3c6933aef8c
Brandon Ewing <brandon.ewing@warningg.com>
2025-01-22 13:10:04 -0600

    Support non-total model testing in helper
    In support of adding details to NTP server dictionaries,
    support detection of non-total TypedDicts and change the logic
    to only error if there are EXTRA keys instead of missing keys.

--- napalm/base/test/helpers.py
+++ napalm/base/test/helpers.py
@@ -4,23 +4,26 @@
 def test_model(model, data):
     """Return if the dictionary `data` complies with the `model`."""
     # Access the underlying schema for a TypedDict directly
-    model = model.__annotations__
-    same_keys = set(model.keys()) == set(data.keys())
+    annotations = model.__annotations__
+    if model.__total__:
+        same_keys = set(annotations.keys()) == set(data.keys())
+    else:
+        same_keys = set(data.keys()) <= set(annotations.keys())
 
     if not same_keys:
         print(
             "model_keys: {}\ndata_keys: {}".format(
-                sorted(model.keys()), sorted(data.keys())
+                sorted(annotations.keys()), sorted(data.keys())
             )
         )
 
     correct_class = True
-    for key, instance_class in model.items():
-        correct_class = isinstance(data[key], instance_class) and correct_class
+    for key in data.keys():
+        correct_class = isinstance(data[key], annotations[key]) and correct_class
         if not correct_class:
             print(
                 "key: {}\nmodel_class: {}\ndata_class: {}".format(
-                    key, instance_class, data[key].__class__
+                    key, annotations[key], data[key].__class__
                 )
             )
 
